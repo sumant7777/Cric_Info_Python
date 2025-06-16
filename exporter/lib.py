2f228763a3de6c149a46b9d92495c219f410fc45
sumant7777 <sumanthnarra7777@gmail.com>
2025-06-16 23:40:53 +0530

    updated sql files and python files

--- exporter/lib.py
+++ exporter/lib.py
@@ -1,5 +1,14 @@
 import json
 from datetime import datetime
+import logging
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
+
 
 def generate_timestamp_format():
     """
@@ -9,22 +18,22 @@ def generate_timestamp_format():
     return datetime.now().strftime("%Y%m%d%H%M%S")
 
 
-def generate_file_name(file_name, file_type, iteration):
+def generate_file_name(file_name: str, file_type: str, iteration: int) -> str:
     """
     this method is used to generate the file name
-    :param file_name: str
-    :param file_type: str
-    :param iteration: int
-    :return: str
+    :param file_name: name of the file
+    :param file_type: type of the file (e.g., json, csv)
+    :param iteration: iteration number for the file
+    :return: complete file name with timestamp and iteration
     """
     return f"data/{file_name}/{generate_timestamp_format()}_{file_name}_{str(iteration).zfill(5)}.{file_type}"
 
 
-def write_to_json(data, file_name):
+def write_to_json(data: json, file_name: str) -> None:
     """
     This method is used to write data to json file
     :param data: json_data
-    :param file_name: str
+    :param file_name: name of the file to write
     """
     try:
         print(f"Writing api data into json file{file_name}")
